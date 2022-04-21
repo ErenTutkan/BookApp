defb8a34ee334e11af9f689b20de9aa25d9f647c
Eren Tutkan <erentutkan@gmail.com>
2022-04-21 14:00:43 +0300

    Chapter Repository and interface added.

---
+++ BookApp.Repository/Repositories/ChapterRepository.cs
@@ -0,0 +1,61 @@
+﻿using BookApp.Core.DTOs.ChapterDtos;
+using BookApp.Core.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Repository.Repositories
+{
+    public class ChapterRepository : IChapterRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ChapterRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<int> Add(ChapterAddRequestDto newChapter)
+        {
+            var command = "func_insert_chapter";
+            var result=await _connection.ExecuteScalarAsync<int>(command,new {c_name=newChapter.ChapterName,c_episode=newChapter.Episode,
+                b_id=newChapter.BookId,c_date=newChapter.CreateDate});
+            return result;
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Chapter>> GetAll()
+        {
+            var query = "select id,chaptername,episode,bookid,create_date createdate from chapters order by createdate desc";
+            var result = await _connection.QueryAsync<Chapter>(query);
+            return result.ToList();
+        }
+
+        public async Task<List<Chapter>> GetByBookId(int bookid)
+        {
+            var query = $"select id,chaptername,episode,bookid,create_date createdate from chapters  where bookid={bookid} order by createdate desc";
+            var result = await _connection.QueryAsync<Chapter>(query);
+            return result.ToList();
+        }
+
+        public async Task<Chapter> GetById(int id)
+        {
+            var query = $"select id,chaptername,episode,bookid,create_date createdate from chapters  where id={id}";
+            var result = await _connection.QuerySingleAsync<Chapter>(query);
+            return result;
+        }
+
+        public Task<bool> Update(ChapterUpdateRequestDto updateChapter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookApp.Repository/Repositories/IChapterRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookApp.Core.DTOs.ChapterDtos;
+using BookApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Repository.Repositories
+{
+    public  interface IChapterRepository
+    {
+        Task<int> Add(ChapterAddRequestDto newChapter);
+        Task<bool> Delete(int id);
+        Task<bool> Update(ChapterUpdateRequestDto updateChapter);
+        Task<List<Chapter>> GetAll();
+        Task<List<Chapter>> GetByBookId(int bookid);
+        Task<Chapter> GetById(int id);
+    }
+}
