e1bf2c470f7db567f80a453d3b63e4888dbb3e02
Eren Tutkan <erentutkan@gmail.com>
2022-04-21 14:01:02 +0300

    Chapter Insert Command and Handler Added.

---
+++ BookApp.Service/Command/Chapter/Insert/ChapterInsertCommand.cs
@@ -0,0 +1,16 @@
+﻿using BookApp.Core.DTOs;
+using BookApp.Core.DTOs.ChapterDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Service.Command.Chapter.Insert
+{
+    public class ChapterInsertCommand:IRequest<ResponseDto<int>>
+    {
+        public ChapterAddRequestDto newChapter { get; set; }
+    }
+}
---
+++ BookApp.Service/Command/Chapter/Insert/ChapterInsertCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BookApp.Core.DTOs;
+using BookApp.Repository.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Service.Command.Chapter.Insert
+{
+    internal class ChapterInsertCommandHandler : IRequestHandler<ChapterInsertCommand, ResponseDto<int>>
+    {
+        private readonly IChapterRepository _chapterRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChapterInsertCommandHandler(IChapterRepository chapterRepository, IUnitOfWork unitOfWork)
+        {
+            _chapterRepository = chapterRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResponseDto<int>> Handle(ChapterInsertCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _chapterRepository.Add(request.newChapter);
+
+            _unitOfWork.Commit();
+            return ResponseDto<int>.Success(result,201);
+        }
+    }
+}
